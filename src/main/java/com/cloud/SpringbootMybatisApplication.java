fb17e7433df07bd657bd3541df71818b65d27600
w_wanghancheng <w_wanghancheng@kingsoft.com>
2018-10-11 16:49:03 +0800

    test-client

--- src/main/java/com/cloud/SpringbootMybatisApplication.java
+++ src/main/java/com/cloud/SpringbootMybatisApplication.java
@@ -1,14 +1,13 @@
 package com.cloud;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.cloud"})
 @EnableTransactionManagement
-@MapperScan("com.cloud.dao")
+//@MapperScan("com.cloud.dao")
 public class SpringbootMybatisApplication {
 
 	public static void main(String[] args) {
